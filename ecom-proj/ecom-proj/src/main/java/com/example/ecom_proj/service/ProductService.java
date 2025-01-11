69ea91fe356350aaf8aaf4ea693e43e0591aa969
DileepaLakshan <145223312+DileepaLakshan@users.noreply.github.com>
2025-01-11 17:49:44 +0530

    create product server and product repo

---
+++ ecom-proj/ecom-proj/src/main/java/com/example/ecom_proj/service/ProductService.java
@@ -0,0 +1,18 @@
+package com.example.ecom_proj.service;
+
+import com.example.ecom_proj.model.Product;
+import com.example.ecom_proj.repo.ProductRepo;
+import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import org.springframework.stereotype.Repository;
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+
+    @Autowired
+    private ProductRepo repo;
+    public List<Product> getAllProducts() {
+        return repo.findAll();
+    }
+}
