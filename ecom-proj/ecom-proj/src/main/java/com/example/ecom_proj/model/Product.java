6932529bfabcded4d187dd640214ca952837881f
DileepaLakshan <145223312+DileepaLakshan@users.noreply.github.com>
2025-01-11 21:01:58 +0530

    new frontend

--- ecom-proj/ecom-proj/src/main/java/com/example/ecom_proj/model/Product.java
+++ ecom-proj/ecom-proj/src/main/java/com/example/ecom_proj/model/Product.java
@@ -1,5 +1,6 @@
 package com.example.ecom_proj.model;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
